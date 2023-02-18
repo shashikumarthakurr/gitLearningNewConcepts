f6c799813f2977b94b18c61e46ab98e389acc059
pulkitkr <pulkitk10@gmail.com>
2023-02-18 13:47:45 +0530

    s

--- src/main/java/com/business/RingPay/RingPayBusinessLogic.java
+++ src/main/java/com/business/RingPay/RingPayBusinessLogic.java
@@ -2788,6 +2788,7 @@ public class RingPayBusinessLogic extends Utilities {
 			Aclick(RingLoginPage.objLoginLink, "Signup/Login link");
 			loginMobile();
 			mobileNoValidation1(mobileNumber);
+			User_Play_Store_Flow(date, roomno, address1, address2, landmark, pin, otp, otp);
 			enterOtp(otp);
 			explicitWaitVisibility(MobileLoginPage.btnReadAndAccept, 10);
 			Aclick(MobileLoginPage.btnReadAndAccept, "Read adn Accept");
