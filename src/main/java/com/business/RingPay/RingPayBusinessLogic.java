cd422d017e19c1214208aa75b575ffcb137b3d0b
pulkitkr <pulkitk10@gmail.com>
2023-02-19 18:42:51 +0530

    werwet

--- src/main/java/com/business/RingPay/RingPayBusinessLogic.java
+++ src/main/java/com/business/RingPay/RingPayBusinessLogic.java
@@ -2784,9 +2784,7 @@ public class RingPayBusinessLogic extends Utilities {
 			String pin=(String) mockUserDetails.get("pin");
 			
 			/*------------------------------Front End----------------------------*/
-			simFlow();
-			kycSkipped(url, genders, encrypted_name, portalEmail, portalPassword, portalOTP);
-			userDetails();
+			addAddress(roomno, address1, address2, landmark, pin);
 			cameraPermission();
 			enablePermissions();
 			Aclick(RingLoginPage.objLoginLink, "Signup/Login link");
