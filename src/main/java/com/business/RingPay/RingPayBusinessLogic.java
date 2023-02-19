a3189fbdc936f0262d1543e5bfe165d82756f418
pulkitkr <pulkitk10@gmail.com>
2023-02-19 19:18:21 +0530

    sdsf

--- src/main/java/com/business/RingPay/RingPayBusinessLogic.java
+++ src/main/java/com/business/RingPay/RingPayBusinessLogic.java
@@ -2784,7 +2784,7 @@ public class RingPayBusinessLogic extends Utilities {
 			String pin=(String) mockUserDetails.get("pin");
 			
 			/*------------------------------Front End----------------------------*/
-			
+			getPlatform();
 			setPlatform("");
 			kycSkipped(url, genders, encrypted_name, portalEmail, portalPassword, portalOTP);
 			userDetails();
