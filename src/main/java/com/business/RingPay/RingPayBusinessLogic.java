ea4db9d35d0e1e66a5ef4f39aee8797c9ec2f0f1
pulkitkr <IGS0502@LAPTOP-3QQ5MNB4>
2023-03-18 18:56:56 +0530

    sgsdgsg

--- src/main/java/com/business/RingPay/RingPayBusinessLogic.java
+++ src/main/java/com/business/RingPay/RingPayBusinessLogic.java
@@ -2802,6 +2802,9 @@ public class RingPayBusinessLogic extends Utilities {
 			mobileNoValidation1(mobileNumber);
 			enterOtp(otp);
 			System.out.println("Shashi Change 2804");
+			System.out.println("Swagatika Change 2804");
+			System.out.println("Swagatika Change 2805");
+
 			explicitWaitVisibility(MobileLoginPage.btnReadAndAccept, 10);
 			Aclick(MobileLoginPage.btnReadAndAccept, "Read adn Accept");
 			explicitWaitVisibility(PermissionPage.btnAllow, 20);
